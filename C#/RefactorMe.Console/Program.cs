78376d1510a47f3a2c8fc3b6e4ad5ee2bf1f708b
Recox <lucas.recoaro@gmail.com>
2023-11-25 18:07:01 +1300

    Added new application console project to use RefactorMe dll as a project reference

---
+++ C#/RefactorMe.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using RefactorMe;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var products = ProductDataConsolidator.Get();
+        var productsEuro = ProductDataConsolidator.GetInEuros();
+        var productsUsd = ProductDataConsolidator.GetInUSDollars();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Products in NZD:");
+        foreach (var product in products)
+        {
+            Console.WriteLine($"Name: {product.Name}, Price: {product.Price}, Type: {product.Type}");
+        }
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("\nProducts in Euros:");
+        foreach (var product in productsEuro)
+        {
+            Console.WriteLine($"Name: {product.Name}, Price: {product.Price}, Type: {product.Type}");
+        }
+
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("\nProducts in USD:");
+        foreach (var product in productsUsd)
+        {
+            Console.WriteLine($"Name: {product.Name}, Price: {product.Price}, Type: {product.Type}");
+        }
+
+        Console.ResetColor();
+    }
+}
